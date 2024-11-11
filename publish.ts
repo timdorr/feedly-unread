87676cbcead048b08095ae9756a322c103edfaa0
Tim Dorr <git@timdorr.com>
2024-11-11 13:37:00 -0500

    Add actual publishing to the script

--- publish.ts
+++ publish.ts
@@ -1,3 +1,20 @@
+import { createReadStream } from 'fs'
+
+import dotenv from 'dotenv'
 import { zip } from 'zip-a-folder'
+import cwu from 'chrome-webstore-upload'
+
+dotenv.config({ path: ['.env.local', '.env'] })
 
 await zip('dist', 'extension.zip')
+const packageFile = createReadStream('./extension.zip')
+
+const store = cwu({
+  extensionId: 'galoemenhjpicpglbfechpddclanflph',
+  clientId: process.env.GOOGLE_CLIENT_ID || '',
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+  refreshToken: process.env.GOOGLE_REFRESH_TOKEN || ''
+})
+
+await store.uploadExisting(packageFile)
+await store.publish()
